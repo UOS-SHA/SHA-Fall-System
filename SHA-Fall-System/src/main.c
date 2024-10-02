22f80139ac8fa9d9d8b3a4794d8961b3b550fa30
신승리 <victory8500@naver.com>
2024-10-02 18:44:19 +0900

    ...

--- SHA-Fall-System/src/main.c
+++ SHA-Fall-System/src/main.c
@@ -16,7 +16,12 @@
 // 프로그램의 entrypoint
 int wmain(void)
 {
+	HANDLE	hStdIn, hStdOut;
+
 	_wsetlocale(LC_ALL, L"Korean");
+
+	hStdIn = GetStdHandle(STD_INPUT_HANDLE);
+	hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
 
 
 }
